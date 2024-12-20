13cb288e53b2012fe077629c0b44770bd8c7c96d
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2024-12-19 16:07:09 -0800

    add commands.none for auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,6 +6,7 @@ import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 
 /**
@@ -42,6 +43,7 @@ public class RobotContainer {
      */
     public Command getAutonomousCommand() {
         // An example command will be run in autonomous
+        // return Commands.none();
         return Autos.exampleAuto(exampleSubsystem);
     }
 }
